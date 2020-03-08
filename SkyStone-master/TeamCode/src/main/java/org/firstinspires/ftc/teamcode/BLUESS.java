ac0d086bce6a3df9a2f41a369377ab2e16f6b7a9
BBIT11397 <bbitrobotics@gmail.com>
2020-03-07 17:10:32 -0800

    Work in Progress Blue Auto

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BLUESS.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BLUESS.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Disabled
 @Autonomous(name="BLUESS", group="bbit")
 public class BLUESS extends LinearOpMode {
     /* Declare OpMode members. */
